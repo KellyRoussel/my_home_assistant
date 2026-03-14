8edfadf342d6444778619676c5a536cc3c69a809
KellyRoussel <kelly.roussel@mines-ales.org>
2026-03-14 15:38:48 +0100

    feat: add internet search tool integration with Tavily API

--- src/tools/tools_library.py
+++ src/tools/tools_library.py
@@ -1,10 +1,13 @@
 from tools.todo_list.add_todo_item_to_list import AddTodoItemToList
 from tools.weather.check_weather import CheckWeather
+from tools.search.internet_search import InternetSearch
 
 add_todo_item_to_list = AddTodoItemToList()
 check_weather = CheckWeather()
+internet_search = InternetSearch()
 
 tools = {
     add_todo_item_to_list.tool_name: add_todo_item_to_list,
     check_weather.tool_name: check_weather,
+    internet_search.tool_name: internet_search,
 }
